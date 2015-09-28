e24f8079724299cedc3138940feb8b025c700f15
Davis Malnieks <malnieks.davis@gmail.com>
2015-09-28 15:49:06 +0300

    a5ultexx: add secureUI blobs

--- a5-common/a5-common-vendor-blobs.mk
+++ a5-common/a5-common-vendor-blobs.mk
@@ -205,6 +205,12 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a5-common/proprietary/vendor/lib/libqmiservices.so:system/vendor/lib/libqmiservices.so \
     vendor/samsung/a5-common/proprietary/vendor/lib/libqmi.so:system/vendor/lib/libqmi.so \
     vendor/samsung/a5-common/proprietary/vendor/lib/libqti-perfd-client.so:system/vendor/lib/libqti-perfd-client.so \
+    vendor/samsung/a5-common/proprietary/vendor/lib/lib-sec-disp.so:system/vendor/lib/lib-sec-disp.so \
+    vendor/samsung/a5-common/proprietary/vendor/lib/libSecureUILib.so:system/vendor/lib/libSecureUILib.so \
+    vendor/samsung/a5-common/proprietary/vendor/lib/libStDrvInt.so:system/vendor/lib/libStDrvInt.so \
+    vendor/samsung/a5-common/proprietary/vendor/lib/libsecureui.so:system/vendor/lib/libsecureui.so \
+    vendor/samsung/a5-common/proprietary/vendor/lib/libsecureui_svcsock.so:system/vendor/lib/libsecureui_svcsock.so \
+    vendor/samsung/a5-common/proprietary/vendor/lib/libsecureuisvc_jni.so:system/vendor/lib/libsecureuisvc_jni.so \
     vendor/samsung/a5-common/proprietary/bin/netmgrd:system/bin/netmgrd \
     vendor/samsung/a5-common/proprietary/bin/qmuxd:system/bin/qmuxd \
     vendor/samsung/a5-common/proprietary/bin/qmiproxy:system/bin/qmiproxy \
