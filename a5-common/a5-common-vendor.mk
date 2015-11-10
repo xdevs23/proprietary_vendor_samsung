3f8e26cab72f7b9084ae2b13013da8478436e135
Davis Malnieks <daavis.90@gmail.com>
2015-11-10 15:36:36 +0200

    a5-common: temp. disable Qcom location service

--- a5-common/a5-common-vendor.mk
+++ a5-common/a5-common-vendor.mk
@@ -17,8 +17,8 @@
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS += vendor/samsung/a5-common/overlay
 
-PRODUCT_PACKAGES += \
-    com.qualcomm.location
+#PRODUCT_PACKAGES += \
+#    com.qualcomm.location
 
 PRODUCT_PACKAGES += \
     libtime_genoff \
